4d19b5b48998d6f8b573ef74ba03d142fd43e8fd
batalov <batalov@inpolus.ru>
2016-02-04 14:13:07 +0300

    update

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""gamepuzzleadmin URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
