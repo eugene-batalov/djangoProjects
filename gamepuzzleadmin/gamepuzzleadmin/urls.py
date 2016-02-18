112551508ad910de2aef87c983227f0da535a867
batalov <batalov@inpolus.ru>
2016-02-18 18:22:18 +0300

    -s

--- gamepuzzleadmin/gamepuzzleadmin/urls.py
+++ gamepuzzleadmin/gamepuzzleadmin/urls.py
@@ -15,20 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
-import os
 
 urlpatterns = [
     url(r'^puzzles/', include('puzzclubadmin.urls', namespace='puzzles')),
     url(r'^admin/', include(admin.site.urls)),
 ]
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL =  '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-
-urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
-urlpatterns += static(MEDIA_ROOT, document_root=MEDIA_ROOT)
